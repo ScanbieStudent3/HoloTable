22fad069faf0f69f409637147b3c4df8007ab002
BoDeKeersmaeker <scanbiestudent3@gmail.com>
2022-06-08 12:32:38 +0200

    HoloTableScript raycast
    Changed the way the holotable raycast works.

--- Assets/Scripts/HoloTableScript.cs
+++ Assets/Scripts/HoloTableScript.cs
@@ -6,7 +6,10 @@ public class HoloTableScript : MonoBehaviour
 {
     [SerializeField] HoloTrackWand holoTrackWand_0 = null;
     [SerializeField] HoloTrackWand holoTrackWand_1 = null;
-    [SerializeField] GameObject LTBuilding = null;
+    [SerializeField] GameObject MainBuilding = null;
+    [SerializeField] GameObject Window = null;
+    [SerializeField] GameObject Car = null;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,23 +20,50 @@ public class HoloTableScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        IsWandPointingAtCollider();
+        if(IsWandPointingAtCollider());
     }
 
-    bool IsWandPointingAtCollider(/*Collider hitbox*/)
+    bool IsWandPointingAtCollider()
     {
-        Ray tempRay = holoTrackWand_0.GetRay();
+        if(HoloTableRayCast(holoTrackWand_0))
+            return true;
+
+        if (HoloTableRayCast(holoTrackWand_1))
+            return true;
+
+        return false;
+    }
+
+    bool HoloTableRayCast(HoloTrackWand tempHoloTrackWand)
+    {
+        Ray tempRay = tempHoloTrackWand.GetRay();
 
         RaycastHit hit;
-
-        if (Physics.Raycast(tempRay.origin, tempRay.direction, out hit, Mathf.Infinity))
+        if (Physics.Raycast(tempRay.origin, tempRay.direction, out hit, Mathf.Infinity) && tempHoloTrackWand.IsButtonAPressed())
         {
-            
+            switch (hit.rigidbody.tag)
+            {
+                case "Building":
+                    //MainBuilding do stuff;
+                    Debug.Log("Building hit");
+                    break;
+
+                case "Window":
+                    //Window do stuff;
+                    Debug.Log("Window hit");
+                    break;
+
+                case "Car":
+                    //Car do stuff;
+                    Debug.Log("Car hit");
+                    break;
+            }
+
+            return true;
         }
 
         Debug.DrawRay(tempRay.origin, tempRay.direction * 10000f, Color.yellow);
 
         return false;
     }
-
 }
