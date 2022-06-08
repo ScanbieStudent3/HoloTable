f2fac02ebcb593167c3c09f334ef07ccf1984550
KasperRuys <kasper_ruys@hotmail.com>
2022-06-08 14:43:01 +0200

    ZoomAnimation added

--- Assets/Scripts/HoloTableScript.cs
+++ Assets/Scripts/HoloTableScript.cs
@@ -6,21 +6,31 @@ public class HoloTableScript : MonoBehaviour
 {
     [SerializeField] HoloTrackWand holoTrackWand_0 = null;
     [SerializeField] HoloTrackWand holoTrackWand_1 = null;
-    [SerializeField] GameObject MainBuilding = null;
-    [SerializeField] GameObject Window = null;
-    [SerializeField] GameObject Car = null;
+    [SerializeField] GameObject MainBuilding;
+    [SerializeField] GameObject Window;
+    [SerializeField] GameObject Car;
+    [SerializeField] Animator animationController;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Car.SetActive(true);
+        Window.SetActive(true);
+        animationController.SetBool("StartZoomToRoom", true);
+        Debug.Log(animationController.GetBool("ZoomToRoom"));
+        Debug.Log("Zzooom");
+        //animateRoomZoom.Play("ZoomToRoom");
+        //animatorRoomZoom.Play("ZoomToRoom");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IsWandPointingAtCollider());
+        //animationController.SetBool("startZoomToRoom", true);
+        if (IsWandPointingAtCollider());
     }
 
     bool IsWandPointingAtCollider()
@@ -39,12 +49,16 @@ public class HoloTableScript : MonoBehaviour
         Ray tempRay = tempHoloTrackWand.GetRay();
 
         RaycastHit hit;
-        if (Physics.Raycast(tempRay.origin, tempRay.direction, out hit, Mathf.Infinity) && tempHoloTrackWand.IsButtonAPressed())
+        if (Physics.Raycast(tempRay.origin, tempRay.direction, out hit, Mathf.Infinity) /*&& tempHoloTrackWand.IsButtonAPressed()*/)
         {
             switch (hit.collider.tag)
             {
                 case "Building":
                     //MainBuilding do stuff;
+                    Debug.Log("Building case triggered");
+                    //animatorRoomZoom.Play("ZoomToRoom");
+                    Car.SetActive(true);
+                    Window.SetActive(true);
                     Debug.Log("Building hit");
                     break;
 
